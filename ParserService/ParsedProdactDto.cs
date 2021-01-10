264d8ab91dfbf3f6ed418292363ecd45303e4b6f
Akimmax <Akimmax>
2021-01-10 18:40:56 +0300

    Add parser service

---
+++ ParserService/ParsedProdactDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProductService.Shared
+{
+
+    public class ParsedProdactDto
+    {
+
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public string ImageSource { get; set; }
+        public string ProductUrl { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+
+        }
+}
